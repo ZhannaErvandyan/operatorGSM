c72ec53c63024ccf143fe9d7cb84e6544e33abe1
ZhannaEr <eruntalle@gmail.com>
2023-10-22 17:59:42 +0300

    java changed

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,22 @@ public class Main {
     public static void main(String[] args) {
 
         int balance = 100;
-        int refill = 300;
+        int refill = 1100;
 
         int bonus = 0;
+        int total = balance + refill;
         if (refill > 1000) {
             bonus = refill / 100;
 
-            int total = balance + refill + bonus;
+            total = balance + refill + bonus;
 
-            System.out.println("Вы пополнили счет  на " + refill + "руб.");
             System.out.println("Ваш бонус составил " + bonus + " руб.");
             System.out.println("Ваш баланс " + total + "руб.");
 
-
         } else {
-            System.out.println("Сумма пополнения должна быть более 1000 руб.");
+            System.out.println("Ваш баланс " + total + "руб.");
+
+
         }
-
-
     }
 }
